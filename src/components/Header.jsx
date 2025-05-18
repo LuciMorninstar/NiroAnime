f6a25a8b5cc04dba465278183f905b50b906cabc
Stars_Winner <stars.winner1121@gmail.com>
2025-05-18 20:18:35 +0545

    Made Navbar responsive for mobile devices only for max-md

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
 
 import MiddleNav from "./MiddleNav";
 import RightNav from "./RigthNav"
+import ResponsiveSearch from "./ResponsiveSearch";
 
 const Header = () => {
   return (
 
     <header>
-        <nav className = "h-[60px] w-full  flex-between px-[15px]">
+        <nav className = "h-[60px] w-full  flex-between px-[15px] max-md:w-full ">
             <img className = "h[80px] w-[80px]" src ="../logo-anime.png " alt="logo"></img>
 
             <MiddleNav/>
 
             <RightNav/>
 
+         
+
+            
+
 
         </nav>
+           <ResponsiveSearch/>
     </header>
 
 
